4af2d2dc7b3213df7cbfccb0bddda6f1fa3a58bf
Felipe Gutierrez <afgutierrez96@gmail.com>
2023-08-14 17:29:43 -0500

    fix the cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,3 @@
-module.exports = {
-    // "baseUrl": "http://localhost:3000",
-    // "chromeDriver": "/path/to/chromedriver",
-    // "viewportWidth": 1920,
-    // "viewportHeight": 1080,
-    // "maxSessions": 1,
-    // "screenshots": true,
-    "videos": true
-  };
+export default {
+  "videos": false
+}
